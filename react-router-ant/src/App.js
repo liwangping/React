c8276047a5cf31c69860dafa6506b96392c4ccc9
lwpwyf <1012312786@qq.com>
2019-06-22 16:03:24 +0800

    react-router-ant

--- react-router-ant/src/App.js
+++ react-router-ant/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
+import {BrowserRouter as Router,
   Route,
   Link
-} from 'react-router-dom';
+} from "react-router-dom";
 import Layout from './page/Layout';
 import logo from './logo.svg';
-import './App.css';
+import 'antd/dist/antd.css';
+// import './App.css';
 
 function App() {
   return (
