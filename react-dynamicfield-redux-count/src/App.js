57e74b9f10748436201001258c1888de836b5862
lwpwyf <1012312786@qq.com>
2019-06-22 21:25:14 +0800

    react-dynamicfield-redux-count

---
+++ react-dynamicfield-redux-count/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import DynamicField from './DynamicField';
+import 'antd/dist/antd.css'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <DynamicField />
+    </div>
+  );
+}
+
+export default App;
